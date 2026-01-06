082e2a5fd0986fca09a9a510631449f168fabc8f
manikanta <155448771+manikantaoruganti@users.noreply.github.com>
2026-01-06 20:12:49 +0530

    Create closure.js

---
+++ closure.js
@@ -0,0 +1,16 @@
+// Demonstrates JavaScript Closure
+
+function counter() {
+  let count = 0; // private variable
+
+  return function () {
+    count++;
+    return count;
+  };
+}
+
+const increment = counter();
+
+console.log(increment()); // 1
+console.log(increment()); // 2
+console.log(increment()); // 3
